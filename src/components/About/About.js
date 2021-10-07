5228148ccdbd54f891a31ab410f2cf00636ad042
Angel-Journey <angel.journey7@gmail.com>
2021-10-06 22:23:43 -0500

    Add About.js file

---
+++ src/components/About/About.js
@@ -0,0 +1,107 @@
+import React from 'react'
+
+export default function About (props) {
+  return (
+    // header container
+    <div>
+
+      {/* steps section */}
+      <div className="ml-4 mr-4 bg-white">
+        <div className="">
+          <h2 className="p-8 text-2xl text-black flex items-center justify-center bg-altbox border-none leading-2 pl-4 pr-4 box-border rounded-lg m-10">Diversify is the brainchild of General Assembly Alum who are each dedicated to championing diversity in the workplace</h2>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-3 gap-4">
+
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+              <div className=" w-80 bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition cursor-pointer">
+                <img src="/images/Christian-Mosely-2.png" alt="Christian Mosely<"></img>
+                <div className="p-5">
+                  <h1 className="text-2xl font-bold">Christian Mosely</h1>
+                  <p className="mt-2 text-lg font-semibold text-gray-600">Software Engineer, Lead PM</p>
+                  <p className="mt-1 text-gray-500 font-">Christian is a web developer from Fort Worth, TX. With a passion for business, sports and technology, Christian decided on a career change during COVID when he found the time to cultivate his passion for web development. Learn more about Christian at these links:</p>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/christian-mosley">
+                  LinkedIn</a></div>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="www.GitHub.com/cmosley">
+                  GitHub</a></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+              <div className=" w-80 bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition cursor-pointer">
+                <img src="/images/Sutherland-Carter.png" alt=""></img>
+                <div className="p-5">
+                  <h1 className="text-2xl font-bold">Sutherland Carter</h1>
+                  <p className="mt-2 text-lg font-semibold text-gray-600">UX/UI, PM</p>
+                  <p className="mt-1 text-gray-500 font-">Hi! I love tech, design systems, and tasty gluten-free pizza.
+                  I am always open to opportunities to positively impact people on a large scale using cutting-edge technology.</p>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.sutherlandcarter.com/">
+                  Portfolio</a></div>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/sutherland-carter/">
+                  LinkedIn</a></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+              <div className=" w-80 bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition cursor-pointer">
+                <img src="/images/Haeree-Lee.png" alt=""></img>
+                <div className="p-5">
+                  <h1 className="text-2xl font-bold">Haeree Lee</h1>
+                  <p className="mt-2 text-lg font-semibold text-gray-600">UX/UI</p>
+                  <p className="mt-1 text-gray-500 font-">Hey, I’m Haeree (hae-dy).
+                  I’m Korean American, a native New Yorker, and I love to make things with my hands.
+                  With a Bachelor’s in anthropology, I love pulling out the anthropologist in me when conducting user research while bringing out the artist in me to design effective wireframes.
+                  I am passionate about content creation and providing safe spaces. I am always looking for ways to evoke certain emotions with my art, whether it’s calm, excitement, happiness, and more.
+                  I also strive to make UX design solutions that solve problems effectively but also give the user joy and calm when using any interface I’ve designed.</p>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.deniseau.com/">
+                  Portfolio</a></div>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/haereelee/">
+                  LinkedIn</a></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+              <div className=" w-80 bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition cursor-pointer">
+                <img src="/images/Denise-Au.png" alt="Denise Au"></img>
+                <div className="p-5">
+                  <h1 className="text-2xl font-bold">Denise Au</h1>
+                  <p className="mt-2 text-lg font-semibold text-gray-600">UX/UI</p>
+                  <p className="mt-1 text-gray-500 font-">My name is Denise, a multidisciplinary designer with a background in events and marketing, currently based in San Francisco after a long ride in Los Angeles, California.
+                  Prior, I studied Psychology at UC Irvine and graduated with a passion for innovation. I spent the next couple years working with start-up companies in marketing, simultaneously freelancing as a wedding planner. My background in cognitive science, combined with my interest in creative design pushes me to problem solve with inclusivity, functionality and aesthetics in mind.
+                  In my free time, I enjoy losing track of time planning outfits I’m too scared to wear outside my room on Pinterest, filling up sketchbooks (but more recently on procreate) with my favorite anime characters, and the occasional photo sesh with my modelesque friends!</p>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.haeleedesign.com/">
+                  Portfolio</a></div>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/deniseau1/">
+                  LinkedIn</a></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+              <div className=" w-80 bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition cursor-pointer">
+                <img src="/images/Angel-Journey-2.png" alt=""></img>
+                <div className="p-5">
+                  <h1 className="text-2xl font-bold">Angel Journey</h1>
+                  <p className="mt-2 text-lg font-semibold text-gray-600">Software Engineer</p>
+                  <p className="mt-1 text-gray-500 font-">Hi! I am an Austin-based Software Developer with a background in customer service and operations management. I am a husband, father, and lover of anything soccer. My desire is to continue strengthening my development skills while working on projects that improve the quality of life for others.</p>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.angeljourney.tech/">
+                  Portfolio</a></div>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/angel-journey/">
+                  LinkedIn</a></div>
+                  <div className="flex justify-center mr-8 underline hover:text-blue-800 visited:text-purple-600"><a rel="noopener noreferrer" target="_blank" href="https://github.com/Angel-Journey">
+                  GitHub</a></div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
